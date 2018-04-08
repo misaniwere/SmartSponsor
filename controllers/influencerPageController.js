8e2c340ad60c5c394cca7ca3e5e4ef67564ca883
Kyra Stephen <kstephen852@hotmail.com>
2018-04-08 09:54:07 -0400

    started working on influencer page

--- controllers/influencerPageController.js
+++ controllers/influencerPageController.js
@@ -3,11 +3,11 @@ var app = express();
 var Influencer = require('../models/influencer');
 
 app.get('/influencerPage', function(req, res) {
-  
+
   Influencer.findOne({
       '_id': req.query.name
   },function(err, results){
-    console.log(req.query.name)
+    console.log("query name" + req.query.name)
    if (err){
      var error = "couldn't load feed";
      res.render('influencerPage', {error: error});
