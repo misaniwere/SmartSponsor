97e6c9b21df95e3b2e250feed0435f6e32aae4ce
Misan Iwere <temisan.iwere@mail.utoronto.ca>
2018-04-08 10:23:39 -0400

    added confirmation page and alert

--- controllers/landingController.js
+++ controllers/landingController.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var Influencer = require('../models/influencer');
 
 app.get('/', function(req, res) {
 	res.render('landing_page');
@@ -7,6 +8,23 @@ app.get('/', function(req, res) {
 
 app.get('/ens_success', function(req, res){
 	res.render('ens_success');
+});
+
+app.get('/confirmation', function(req, res){
+	Influencer.findOne({
+      '_id': req.query.name
+  },function(err, results){
+    console.log("query name" + req.query.name)
+   if (err){
+     var error = "couldn't load feed";
+     res.render('confirmation', {error: error});
+		 console.log(error);
+   }
+   else{
+     res.render('confirmation', {data: results});
+		 console.log(results);
+   }
+   });
 });
 
 module.exports = app;
